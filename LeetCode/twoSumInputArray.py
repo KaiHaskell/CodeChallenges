94d3be95a44ba655e1809df9676eba4fddf15623
Kai Haskell <me@kaihaskell.com>
2020-08-18 16:50:58 -0400

    Added twoSumInputArray.py

---
+++ LeetCode/twoSumInputArray.py
@@ -0,0 +1,21 @@
+# Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+# The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+# Note:
+
+# Your returned answers (both index1 and index2) are not zero-based.
+# You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+            elif numbers[left] + numbers[right] < target:
+                left += 1
