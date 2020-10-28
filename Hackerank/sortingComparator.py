cfe81f9779b7524cc5f263cf2553542397dd9943
Kai Haskell <me@kaihaskell.com>
2020-10-27 22:06:26 -0400

    sortingComparator.py

---
+++ Hackerank/sortingComparator.py
@@ -0,0 +1,46 @@
+from functools import cmp_to_key
+
+
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def __repr__(self):
+        return ""
+
+    def comparator(a, b):
+        # compare scores
+        if a.score > b.score:
+            return -1
+        elif a.score < b.score:
+            return 1
+            # if a's score is higher
+            # return -1
+            # if b'a score is higher
+            # return 1
+
+        if a.name > b.name:
+            return 1
+        elif a.name < b.name:
+            return -1
+    # compare the names
+        # if a's name is less than b's name
+            # return -1
+        # if b's name is less than a's name
+            # return 1
+
+        return 0
+
+
+n = int(input())
+data = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
+
+data = sorted(data, key=cmp_to_key(Player.comparator))
+for i in data:
+    print(i.name, i.score)
