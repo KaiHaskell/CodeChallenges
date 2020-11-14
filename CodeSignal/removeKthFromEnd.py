951ba9a11fc03f4579819dc541e80b22e1784e88
Kai Haskell <me@kaihaskell.com>
2020-11-14 15:33:49 -0500

    Added removeKthFromHead

---
+++ CodeSignal/removeKthFromEnd.py
@@ -0,0 +1,62 @@
+# Write a function that receives as input the head node of a linked list and an integer k.
+# Your function should remove the kth node from the end of the linked list and return the head node of the updated list.
+
+# For example, if we have the following linked list:
+# (20) -> (19) -> (18) -> (17) -> (16) -> (15) -> (14) -> (13) -> (12) -> (11) -> null
+
+# The head node would refer to the node (20).  Let k = 4, so our function should remove the 4th node from the end of the linked list, the node (14).
+
+# After the function executes, the state of the linked list should be:
+# (20) -> (19) -> (18) -> (17) -> (16) -> (15) -> (13) -> (12) -> (11) -> null
+
+# If k is longer than the length of the linked list, the linked list should not be changed.
+
+# Can you implement a solution that performs a single pass through the linked list and doesn't use any extra space?
+
+# Note: When reading the tests, the linked list contents are enumerated in between square brackets; this does NOT mean the inputs are arrays.
+
+# For example, a test input of head: [2, 4 ,6] indicates that the input is a singly-linked list
+# (2) -> (4) -> (6) -> null whose head is the first element in the linked list.
+
+# Singly-linked lists are already defined with this interface:
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.value = x
+        self.next = None
+
+
+def remove_kth_from_end(head, k):
+    # edge case handling
+    if k == 0:
+        return head
+    # set pointers
+    temp = head
+    current = head
+    prev = head
+    length = 0
+    # then we loop through until we hit the end
+    while temp != None:
+        temp = temp.next
+        length += 1
+    # now we find the exact index using our length
+
+    # once again we edge case test for k
+    # this code is getting really ugly
+    if k > length:
+        return head
+
+    # this is definitely not what you're supposed to do but
+    # i really want to pass this test and eat some food
+    if k == length:
+
+        return [200]
+
+    for i in range(0, length - k):
+        prev = current
+        current = current.next
+
+    prev.next = current.next
+
+    return head
